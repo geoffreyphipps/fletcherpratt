789da5e659c427d628ac4c57a7908f7c3c56a376
gphipps <gphipps@gphipps.com>
2021-10-01 21:43:38 -0700

    Doubled damage

--- src/com/gphipps/fletcherpratt/GunType.java
+++ src/com/gphipps/fletcherpratt/GunType.java
@@ -17,51 +17,51 @@ public class GunType {
         gunTypes.put("1.5", new GunType(1.5, 1, 6));
         gunTypes.put("1.6", new GunType(1.6, 1, 6));
 
-        gunTypes.put("2.0", new GunType(2, 1, 6));
+        gunTypes.put("2.0", new GunType(2, 2, 6));
 
-        gunTypes.put("3.0", new GunType(3.0, 2, 10));
-        gunTypes.put("3.4", new GunType(3.4, 3, 20));
-        gunTypes.put("3.5", new GunType(3.5, 3, 20));
-        gunTypes.put("3.9", new GunType(3.9, 4, 11));
+        gunTypes.put("3.0", new GunType(3.0, 5, 10));
+        gunTypes.put("3.4", new GunType(3.4, 6, 20));
+        gunTypes.put("3.5", new GunType(3.5, 6, 20));
+        gunTypes.put("3.9", new GunType(3.9, 8, 11));
 
-        gunTypes.put("4.0", new GunType(4, 4, 11));
-        gunTypes.put("4.1", new GunType(4.1, 5, 11));
-        gunTypes.put("4.5", new GunType(4.5, 5, 12));
-        gunTypes.put("4.7", new GunType(4.7, 6, 13));
+        gunTypes.put("4.0", new GunType(4, 9, 11));
+        gunTypes.put("4.1", new GunType(4.1, 10, 11));
+        gunTypes.put("4.5", new GunType(4.5, 11, 12));
+        gunTypes.put("4.7", new GunType(4.7, 12, 13));
 
-        gunTypes.put("5.0", new GunType(5, 7, 14));
-        gunTypes.put("5.1", new GunType(5.1, 9, 14));
-        gunTypes.put("5.2", new GunType(5.2, 9, 15));
-        gunTypes.put("5.5", new GunType(5.5, 10, 15));
-        gunTypes.put("5.9", new GunType(5.9, 12, 18));
+        gunTypes.put("5.0", new GunType(5, 13, 14));
+        gunTypes.put("5.1", new GunType(5.1, 18, 14));
+        gunTypes.put("5.2", new GunType(5.2, 19, 15));
+        gunTypes.put("5.5", new GunType(5.5, 20, 15));
+        gunTypes.put("5.9", new GunType(5.9, 24, 18));
 
-        gunTypes.put("6.0", new GunType(6, 13, 18));
-        gunTypes.put("6.1", new GunType(6.1, 14, 19));
+        gunTypes.put("6.0", new GunType(6, 25, 18));
+        gunTypes.put("6.1", new GunType(6.1, 28, 19));
 
-        gunTypes.put("7.5", new GunType(7.5, 22, 21));
+        gunTypes.put("7.5", new GunType(7.5, 44, 21));
 
-        gunTypes.put("8.0", new GunType(8, 29, 22));
+        gunTypes.put("8.0", new GunType(8, 58, 22));
 
-        gunTypes.put("9.2", new GunType(9.2, 38, 23));
-        gunTypes.put("9.4", new GunType(9.4, 41, 24));
+        gunTypes.put("9.2", new GunType(9.2, 76, 23));
+        gunTypes.put("9.4", new GunType(9.4, 83, 24));
 
-        gunTypes.put("10.0", new GunType(10, 49, 25));
+        gunTypes.put("10.0", new GunType(10, 98, 25));
 
-        gunTypes.put("11.0", new GunType(11, 65, 27));
+        gunTypes.put("11.0", new GunType(11, 130, 27));
 
-        gunTypes.put("12.0", new GunType(12, 93, 29));
+        gunTypes.put("12.0", new GunType(12, 185, 29));
 
-        gunTypes.put("12.6", new GunType(12.6, 118, 30));
+        gunTypes.put("12.6", new GunType(12.6, 235, 30));
 
-        gunTypes.put("13.0", new GunType(13, 129, 31));
+        gunTypes.put("13.0", new GunType(13, 258, 31));
 
-        gunTypes.put("13.5", new GunType(13.5, 130, 31));
+        gunTypes.put("13.5", new GunType(13.5, 260, 31));
 
-        gunTypes.put("14.0", new GunType(14, 156, 32));
+        gunTypes.put("14.0", new GunType(14, 313, 32));
 
-        gunTypes.put("15.0", new GunType(15, 214, 33));
+        gunTypes.put("15.0", new GunType(15, 428, 33));
 
-        gunTypes.put("16.0", new GunType(16, 264, 34));
+        gunTypes.put("16.0", new GunType(16, 528, 34));
     }
 
     public GunType(double bore, int damage, int maxRange) {
